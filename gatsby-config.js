30037f32c35d5d7d5115c3b182dab8be8ab8f129
SofiaMEweiss <sofia.weiss@iths.se>
2021-11-26 14:37:26 +0100

    Added access to contentful and started to create Graphql queries.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,22 @@
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "blogglabb1",
-  },
-  plugins: [],
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "blogg-contentful",
+    },
+    plugins: [
+        "gatsby-plugin-sharp",
+        "gatsby-plugin-image",
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: `rb8rhzapqadb`,
+                accessToken: `hJz671-i00fEytGLky-DtR_sV3z9MSXYTKShXQpzejk`,
+                // accessToken: process.env.COONTENTFUL_ACCESS_TOKEN,
+            },
+        },
+    ],
 };
